9311ea17122d6600b0a0362cf8b3d902c85e55b4
Muhammad Juan Akbar <mail@mjuan.me>
2017-10-17 23:43:03 +0200

    allow used heroku db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
 return [
 
     /*
@@ -41,11 +43,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => isset($db['host']) ?:env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => substr($db["path"], 1) ?:env('DB_DATABASE', 'forge'),
+            'username' => isset($db['user']) ?:env('DB_USERNAME', 'forge'),
+            'password' => isset($db['pass']) ?:env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
