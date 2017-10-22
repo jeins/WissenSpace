49046d04819063c18815ba80d79ac35d971cb60e
Muhammad Juan Akbar <mail@mjuan.me>
2017-10-22 23:22:26 +0200

    cleanup db config

--- config/database.php
+++ config/database.php
@@ -1,7 +1,5 @@
 <?php
 
-$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
 return [
 
     /*
@@ -43,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => isset($db['host']) ?:env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => substr($db["path"], 1) ?:env('DB_DATABASE', 'forge'),
-            'username' => isset($db['user']) ?:env('DB_USERNAME', 'forge'),
-            'password' => isset($db['pass']) ?:env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
